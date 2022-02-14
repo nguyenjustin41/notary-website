97e9605a20ec280e6b0038247162f18ee211731f
nguyenjustin41 <53131589+nguyenjustin41@users.noreply.github.com>
2022-02-13 23:21:59 -0800

    fixed ui issues with IPAD

--- client/notary_website/src/components/Contact/Contact.jsx
+++ client/notary_website/src/components/Contact/Contact.jsx
@@ -97,13 +97,13 @@ const Contact = () => {
 
   return (
 
-      <div id="contact" className="mobile-contact-container flex flex-col items-center relative h-[100vh] w-[100%] p-[5%] px-[30%] gap-y-[10%]">
+      <div id="contact" className="mobile-contact-container flex flex-col items-center relative h-[100%] w-[100%] bg-[#f4f4f4] p-[5%]">
             <div className="contact-waves">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
                 </svg>
             </div>
-            <h1 className="mobile-header -tracking-[0.64px] text-[64px] leading-[52px] font-bold text-black text-shadow">Contact Form</h1>
+            <h1 className="mobile-header -tracking-[0.64px] text-[64px] leading-[52px] font-bold text-black text-shadow mb-[10%] xl:mb-[5%]">Contact Form</h1>
             {
                 submitted ? 
                     <div className="flex flex-col justify-between items-center bg-white rounded-2xl shadow-lg p-[5%] gap-y-[5%] mt-[0%] sm:mt-[10%]  md:mt-[5%] ">  
@@ -115,34 +115,34 @@ const Contact = () => {
                         </div>
                         
                     </div> :
-                    <div className="flex flex-row justify-center items-center w-[100%] bg-white rounded-[5px] shadow-lg h-full mt-[0%] sm:mt-[10%]  md:mt-[5%]  ">
+                    <div className="flex flex-row justify-center items-center w-[100%] xl:w-[50%] lg:scale-110 bg-[#f4f4f4] rounded-[5px] shadow-lg h-[100%]">
                         {/* container to hold all form inputs */}
-                        <form onSubmit={(e) => handleSubmit(e)} className="mobile-contact-form flex flex-col w-[100%] justify-center items-center h-[100%] p-[8%] gap-y-[5%]  xl:gap-y-[3%] xl:p-[5%]">
+                        <form onSubmit={(e) => handleSubmit(e)} className="mobile-contact-form flex flex-col w-[100%] justify-around items-between h-[100%]">
                             {/* name & company div */}
-                            <div className='mobile-contact-info flex flex-row h-[100%] w-[100%] gap-y-[2%] gap-x-[2%]'>
+                            <div className='mobile-contact-info flex flex-row h-[100%] w-[100%]'>
 
-                                <div className="mobile-contact-info w-[50%] h-[100%]">
-                                    <input id="name" value={data.name} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" type='text' placeholder='*Name' required/>
+                                <div className="mobile-contact-info w-[50%] h-[100%] pb-[2%] ">
+                                    <input id="name" value={data.name} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[2rem]" type='text' placeholder='*Name' required/>
 
                                 </div>
-                                <div className="mobile-contact-info  w-[50%] h-[100%]">
-                                    <input id="company" value={data.company}  onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" type='text' placeholder='Company'/>
+                                <div className="mobile-contact-info  w-[50%] h-[100%] pb-[2%]">
+                                    <input id="company" value={data.company}  onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[2rem]" type='text' placeholder='Company'/>
 
                                 </div>
                             </div>
                             {/* email & phone div */}
-                            <div className='mobile-contact-info flex flex-row justify-center h-[100%] w-[100%] gap-x-[2%]'> 
-                                <div className="mobile-contact-info  w-[50%] h-[100%]">
-                                    <input id="email"  value={data.email} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" type='email' placeholder='*Email' required/>
+                            <div className='mobile-contact-info flex flex-row justify-center h-[100%] w-[100%] '> 
+                                <div className="mobile-contact-info  w-[50%] h-[100%] pb-[2%]">
+                                    <input id="email"  value={data.email} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[2rem]" type='email' placeholder='*Email' required/>
 
                                 </div>
-                                <div className="mobile-contact-info w-[50%] h-[100%]">
-                                    <input id="phone" value={data.phone} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" type='text' placeholder='Phone'/>
+                                <div className="mobile-contact-info w-[50%] h-[100%] pb-[2%]">
+                                    <input id="phone" value={data.phone} onChange={(e) => handleDataChange(e)} className="px-[5%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[2rem]" type='text' placeholder='Phone'/>
 
                                 </div>
                             </div>
-                            <div className='mobile-contact-select h-[100%] w-[100%] bg-grey-color rounded-2xl'>
-                                <select id="mortgage" onChange={(e) => handleDataChange(e)} className="px-[2%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" placeholder="Select Mortgage Package" name="types">
+                            <div className='mobile-contact-select h-[100%] w-[100%] rounded-2xl '>
+                                <select id="mortgage" onChange={(e) => handleDataChange(e)} className="px-[2%] w-[100%] h-[100%] pb-[2%] bg-grey-color border-solid border-2 text-[1.5rem] lg:text-[2rem] " placeholder="Select Mortgage Package" name="types">
                                 
                                     {   
                                         // map(element, index) => [...]
@@ -151,12 +151,12 @@ const Contact = () => {
                                     
                                 </select>
                             </div>
-                            <div className='h-[100%] w-[100%] bg-grey-color '>
-                                <textarea id="message" value={data.message} onChange={(e) => handleDataChange(e)} name="message" cols="40" rows="4" className="py-[2%] px-[2%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[1.5rem]" type='text' placeholder='*Message'></textarea>
+                            <div className='h-[100%] w-[100%] '>
+                                <textarea id="message" value={data.message} onChange={(e) => handleDataChange(e)} name="message" cols="40" rows="4" className="mt-[2%] px-[2%] pb-[2%] w-[100%] h-[100%] bg-grey-color border-solid border-2 text-[2rem]" type='text' placeholder='*Message'></textarea>
                             </div>
 
-                            <div className='mobile-contact-submit flex flex-row h-[100%] w-[100%] '>
-                                <button type="submit" className="flex flex-row justify-center items-center w-[100%] shadow-lg rounded-[px] font-semibold text-[28px] text-white outline-hidden cursor-pointer transition-all ease-in-out duration-200 bg-button-color whitespace-nowrap px-[22px] py-[10px] text-shadow hover:bg-button-color-hover">Submit</button>
+                            <div className='mobile-contact-submit flex flex-row h-[100%] w-[100%]'>
+                                <button type="submit" className="flex flex-row justify-center items-center w-[100%] shadow-lg rounded-[px] font-semibold text-[2rem] text-white outline-hidden cursor-pointer transition-all ease-in-out duration-200 bg-button-color whitespace-nowrap px-[22px] py-[10px] text-shadow hover:bg-button-color-hover">Submit</button>
                             </div>
 
                         </form>
